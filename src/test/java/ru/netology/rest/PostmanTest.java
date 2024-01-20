1eb06515df1cd1b92e033977d4e76be8bb01066c
RayanGosling <mr.avdonin@inbox.ru>
2024-01-20 12:32:10 +0300

    Broken test

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -19,7 +19,7 @@ class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some dta"))
         ;
     }
 }
